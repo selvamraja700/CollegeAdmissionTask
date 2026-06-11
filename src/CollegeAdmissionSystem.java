57128128628a1a25030ab440b5507ab79cee3a88
selvam <selvamraja700@gmail.com>
2026-06-11 12:28:52 +0530

    CollegeAdmission

--- src/CollegeAdmissionSystem.java
+++ src/CollegeAdmissionSystem.java
@@ -6,7 +6,7 @@ public class CollegeAdmissionSystem {
 
     static final String DB_URL = "jdbc:mysql://localhost:3306/college_admission";
     static final String DB_USER = "root";
-    static final String DB_PASS = "PassWord"; // Replace with your MySQL password
+    static final String DB_PASS = "PassWord"; 
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
